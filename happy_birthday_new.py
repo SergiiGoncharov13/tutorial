304eab297adca2a95e85c2175fb8a923592515d0
SergiiGoncharov13 <112499198+SergiiGoncharov13@users.noreply.github.com>
2022-11-20 12:56:51 +0200

    Add files via upload

--- happy_birthday_new.py
+++ happy_birthday_new.py
@@ -1,35 +1,29 @@
 from datetime import timedelta, datetime
 
 users = [
-    {"name": "Bill", "birthday": datetime(1992, 11, 23)},
-    {"name": "Kate", "birthday": datetime(2000, 11, 22)},
-    {"name": "John", "birthday": datetime(2001, 11, 24)},
-    {"name": "Ann", "birthday": datetime(1999, 11, 23)},
-    {"name": "Oleg", "birthday": datetime(2002, 12, 12)},
-    {"name": "Vlad", "birthday": datetime(1995, 11, 30)},
-    {"name": "Frank", "birthday": datetime(1999, 11, 16)}
+    {"name": "Bill", "birthday": datetime(year=1992, month=11, day=23)},
+    {"name": "Kate", "birthday": datetime(year=2000, month=11, day=22)},
+    {"name": "John", "birthday": datetime(year=2001, month=1, day=24)},
+    {"name": "Ann", "birthday": datetime(year=1999, month=11, day=23)},
+    {"name": "Oleg", "birthday": datetime(year=2002, month=12, day=12)},
+    {"name": "Vlad", "birthday": datetime(year=1995, month=11, day=30)},
+    {"name": "Frank", "birthday": datetime(year=1999, month=11, day=20)}
 ]
 
-today_date = datetime.now()
-if today_date.weekday() == 5:
-    days_interval = timedelta(days=6)
-elif today_date.weekday() == 6:
-    days_interval = timedelta(days=5)
-else:
-    days_interval = timedelta(days=7)
-new_day = today_date + days_interval
 
-
-def get_birthdays_per_week(users):
+def get_birthdays_per_week(usr):
+    today_date = datetime.now()
+    days_interval = define_days_interval(today_date)
+    new_day = today_date + days_interval
 
     day_of_week = {
-        "monday": [],
-        "tuesday": [],
-        "wednesday": [],
-        "thursday": [],
-        "friday": [],
-        "saturday": [],
-        "sunday": []
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [],
+        "Sunday": []
     }
     for user in users:
         new_date = datetime(
@@ -40,17 +34,26 @@ def get_birthdays_per_week(users):
 
         if today_date < new_date <= new_day:
             weekday_string = new_date.strftime("%A")
-            if weekday_string in ["saturday", "sunday"]:
+            if weekday_string in ["Saturday", "Sunday"]:
                 weekday_string = "monday"
             day_of_week.get(weekday_string).append(user.get("name"))
     return print_result_list(day_of_week)
+
+
+def define_days_interval(today_date):
+    if today_date.weekday() == 5:
+        days_interval = timedelta(days=6)
+    elif today_date.weekday() == 6:
+        days_interval = timedelta(days=5)
+    else:
+        days_interval = timedelta(days=7)
+    return days_interval
 
 
 def print_result_list(day_of_week):
     for k, v in day_of_week.items():
         if v:
             print(f'{k}: {", ".join(v)}')
-
 
 
 if __name__ == "__main__":
